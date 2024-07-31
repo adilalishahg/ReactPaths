d6675f7c8d1347b296e9ad20c2cead3b4d4c062d
adilalishahg <adilalishahg@gmail.com>
2024-07-31 15:23:28 -0700

    [Routing][Another][Method]

--- routing-custom-hooks/src/App.jsx
+++ routing-custom-hooks/src/App.jsx
@@ -1,9 +1,37 @@
-import { Route, Routes, useNavigate } from "react-router-dom"
+import { Route, Routes, useNavigate, useRoutes } from "react-router-dom"
 import Reciepe from "./pages/reciepe"
 import Comment from "./pages/comment"
 import ReciepeDetailPage from "./pages/reciepe-details"
 import NotFoundPage from "./pages/not-found"
 import Layout from "./components/layout"
+
+function RoutesNew(){
+  const element = useRoutes([
+    {
+      path:"/home",
+      element: <Layout/>,
+      children: [
+        {
+          path: "reciepe-list",
+          element: <Reciepe/>
+        },
+        {
+          path: "comments",
+          element: <Comment/>
+        },
+        {
+          path: "reciepe-list/:id",
+          element: <ReciepeDetailPage/>
+        },
+        {
+          path: "*",
+          element: <NotFoundPage/>
+        }
+      ]
+    }    
+  ])
+  return element
+}
 
 function App() { 
   const navigate = useNavigate()
@@ -18,14 +46,16 @@ function App() {
         onClick={() => navigate("/home/comments")}
       >Comments</button> 
 
-      <Routes>
+      {/* <Routes>
         <Route path="/home" element={<Layout/>}>
           <Route path="reciepe-list" element={<Reciepe />}/>
           <Route path="comments" element={<Comment />}/>        
           <Route path="reciepe-list/:id" element={<ReciepeDetailPage />}/>
         </Route>
         <Route path="*" element={<NotFoundPage />}/>
-      </Routes>
+      </Routes> */}
+      <RoutesNew/>
+
     </>
   )
 }
